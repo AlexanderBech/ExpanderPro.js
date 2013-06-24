48a29a3536ce763b73b25d66c2b0ee46d571dd13
Alexander Bech <ab@bechventures.com>
2013-06-24 13:11:55 +0200

    Initial commit

---
+++ jquery-expanderpro.js
@@ -0,0 +1,58 @@
+/* =========================================================
+*	ExpanderPro 1.0
+*	Author: Alexander Bech / www.alexanderbech.com
+*	https://github.com/AlexanderBech/jquery-expanderpro
+* ========================================================== */
+(function ($){
+	var _default = {
+		expandItem: "expandee",		// Item class
+		triggerClass: "trigger",	// Trigger class
+		targetClass: "target",		// Target class
+		expandedClass: "expanded",	// Expanded class
+		hideTrigger: false,			// If true, will hide trigger on select
+		animSpeed: 500,				// Animation speed in miliseconds
+		hoverClass: "over",			// If not null, will add class on hover
+		closeOthers: false,			// If true, will close other elements than selected,
+		startExpanded: false,		// If true, target will start expanded
+		preventClosing: false,		// If true, expanded item will not close on trigger
+		openFirst: false			// If true, first item will open at init
+	};
+	$.fn.expanderPro = function(options){
+  		var _opts = $.extend({}, _default, options);
+  		return this.each(function(){
+  			var $container = $(this);
+  			var $items = $('.'+_opts.expandItem, $container);
+  			$items.each(function(index){
+  				var $item = $(this),
+  					$trigger = _getTrigger($item),
+	  				$target = _getTarget($item);
+  				// Hide target?
+	  			if(!_opts.startExpanded){ $target.hide(); } else { $target.show(); $item.addClass(_opts.expandedClass); }
+	  			// Open first?
+	  			if(_opts.openFirst && index==0){ $target.show(); $item.addClass(_opts.expandedClass); }
+	  			// Hover class
+	  			if(_opts.hoverClass != null) $item.on({mouseenter:function(){$item.addClass(_opts.hoverClass)},mouseleave:function(){$item.removeClass(_opts.hoverClass)}});
+	  			// Trigger
+	  			$trigger.on("click", function(e){ e.preventDefault();
+	  				// Hide trigger?
+	  				if(_opts.hideTrigger) $trigger.hide();
+	  				// Close others?
+	  				if(_opts.closeOthers){ $('.'+_opts.targetClass, $container).stop().slideUp(_opts.animSpeed); $('.'+_opts.expandItem, $container).removeClass(_opts.expandedClass); }
+	  				// Anim target
+	  				if(_opts.preventClosing){ $target.stop().slideDown(_opts.animSpeed); $item.addClass(_opts.expandedClass);
+	  				} else { $target.stop().slideToggle(_opts.animSpeed,function(){$item.addClass(_opts.expandedClass)}); }
+	  			});
+  			});
+  		});
+		// Get trigger
+		function _getTrigger($item){
+			if('.'+_opts.triggerClass != null) return $('.'+_opts.triggerClass, $item);
+			else return $item;
+		}
+		// Get target
+		function _getTarget($item){
+			if('.'+_opts.targetClass != null) return $('.'+_opts.targetClass, $item);
+			else return $item;
+		} 
+  	};
+})(jQuery);
